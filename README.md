a922c0f11264660fa3aeea2e367f7ba4b9bab03d
WARFADE <96909243+WARFADE-BOSS@users.noreply.github.com>
2022-07-07 00:00:20 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# Bitly Link Shortner Bot 
+
+>[Generate Shorter Links Easily From Here](https://telegram.me/Bitly_Link_Create_BOT)
+
+<p align="center"><a href="https://www.github.com/WARFADE-BOSS/BitlyBOT"><img src="https://telegra.ph/file/d66b376f2eaf746c7117e.jpg" width="2000"></a></p>
+
+<h6 align="center">
+<b>⭐ A Telegram Bot To Shortern The Lengthy Links ⭐<b>
+</h6>
+
+
+<details>
+
+<summary> • ✨ Deploy to Heroku ✨</summary>
+
+--------
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/WARFADE-BOSS/BitlyBOT)
+
+--------
+</details>
+
+<details><summary>• 🔰 Mandatory Vars 🔰</summary>
+
+---------
+- `API_ID` - Get This Value From [Here](https://my.telegram.org/auth)
+- `API_HASH` - Get This From [Here](https://my.telegram.org/auth)
+- `BOT_TOKEN` - Get This From [Here](https://telegram.me/BotFather)
+- `BITLY_TOKEN` - Get This From [Here](https://bitly.is/accesstoken)
+---------
+</details>
---
+++ bot.py
@@ -0,0 +1,38 @@
+from pyrogram import Client, filters
+import requests 
+import json 
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+API_ID = int(os.environ.get("API_ID",12345))
+API_HASH = os.environ.get("API_HASH","")
+BITLY_TOKEN = os.environ.get("BITLY_TOKEN","")
+
+headers = {
+    'Authorization': BITLY_TOKEN,
+    'Content-Type': 'application/json',
+}
+
+
+app = Client("bitlybot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
+
+@app.on_message(filters.private & filters.command(['start']))
+async def start(client,message):
+  await message.reply_text(f"**Hey {message .from_user.first_name},**\n\nWelcome to WarFade's Link Shortner Bot. \n\n**Send Any Link That You Want To Shorten**\n\nMade With ♥️ By @WarFade", reply_to_message_id = message.message_id)
+  
+@app.on_message(filters.private & filters.regex("http|https"))
+async def Bitly(client,message):
+  URL = message.text
+  DOMAIN = "bit.ly"
+  value  = {'long_url': URL , 'domain': DOMAIN}
+  data = json.dumps(value)
+  try:
+    r = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers,data = data )
+    result = r.json()
+    link = result["link"]
+    await message.reply_text(f"**Hey {message .from_user.first_name} , Your Shortened Url Is**\n\n{link}\n\nMade With ♥️ By @WarFade", reply_to_message_id= message.message_id)
+  except Exception as e :
+    await message.reply_text(e)
+    
+app.run()
+    
